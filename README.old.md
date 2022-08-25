a64a02a35e02563a69ddcaf69799d8eafd23610c
gon araseon <72021422+ravikul505@users.noreply.github.com>
2022-08-25 22:41:47 +0530

    3.

--- README.old.md
+++
@@ -1,4 +0,0 @@
-# HacklabAssignment1
-assignement project by hacklab solutions
-
-A demo webiste which uses FSM to control a traffic light 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
-import * as React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+// import TrafficLight from "./components/TrafficLight";
+import TrafficLight from "./components/TrafficLightAutomata";
 
-import './styles.css';
+import "./styles.css";
 
 function App() {
-  const [counter, setCounter] = React.useState(3);
-
-
-  React.useEffect(() => {
-    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
-  }, [counter]);
-  if (counter == 0) {
-    console.log('a')
-  }
-  return (
-    <div className="App">
-      <button type="button" class="button1">Countdown: {counter}</button>
-      <button type="button" class="button2">Countdown: {counter}</button>
-      <button type="button" class="button3">Countdown: {counter}</button>
-    </div>
-  );
+  return <TrafficLight />;
 }
 
-const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
