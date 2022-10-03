f97bd78248394dadf8663b966179c04ef5ede125
TusharVaid30 <tusharvaid30@gmail.com>
2022-10-03 15:57:10 +0530

    yes

--- src/components/Auto.js
+++ src/components/Auto.js
@@ -1,29 +1,68 @@
-import { createMachine, interpret, assign } from 'xstate';
-import Red from './components/Red'
-import Yellow from './components/Yellow'
-import Green from './components/Green'
+import Red from './Red'
+import Yellow from './Yellow'
+import Green from './Green'
+import React from 'react';
 
-function Auto() {
-    const lightMachine = createMachine({
-        id: 'lightMachine',
-        initial: 'green',
-        states: {
-            GREEN: {
-                on: {
-                    NEXT: 'YELLOW'
-                }
-            },
-            YELLOW: {
-                on: {
-                    NEXT: 'RED'
-                }
-            },
-            RED: {
-                on: {
-                    NEXT: 'GREEN'
-                }
-            }
+class Auto extends React.PureComponent {
+    state = {
+        green: true,
+        yellow: false,
+        red: false
+    };
+
+    transition = () => {
+        if (this.state.green) {
+            this.setState({
+                green: false,
+                yellow: true,
+                red: false
+            });
+            this.waitLongInterval();
+            return;
         }
-    });
+
+        if (this.state.yellow) {
+            this.setState({
+                green: false,
+                yellow: false,
+                red: true
+            });
+            this.waitLongInterval();
+            return;
+        }
+
+        if (this.state.red) {
+            this.setState({
+                green: true,
+                yellow: false,
+                red: false
+            });
+            this.waitLongInterval();
+            return;
+        }
+    }
+
+    waitLongInterval() {
+        setTimeout(this.transition, 1000)
+    }
+
+    waitShortInterval() {
+        setTimeout(this.transition, 500)
+    }
+
+    componentDidMount() {
+        this.waitLongInterval();
+    }
+
+    render() {
+        const { green, yellow, red } = this.state;
+        return (
+            <div>
+                <Red isOn={red} />
+                <Yellow isOn={yellow} />
+                <Green isOn={green} />
+            </div>
+        )
+    }
 }
 export default Auto
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import Red from './components/Red'
-import Yellow from './components/Yellow'
-import Green from './components/Green'
+
+import Auto from './components/Auto'
 import './styles.css';
 
 function App() {
   return (<div className='hello'>
-    <Red />
-    <Green />
-    <Yellow />
+    <Auto />
 
   </div >
   )
